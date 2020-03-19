b3236b70eb3f82c5c78ab5b70dcf31884223f339
mikejeter <mjeter45@yahoo.com>
2020-03-19 19:36:24 -0400

    Writing endpoints and middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const db = require('./postDb.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // do your magic!
+  db.get()
+    .then(post => {
+        res.status(200).json({post});
+    })
+    .catch(err => {
+    res.status(500).json({success:false, err: 'The posts information could not be retrieved.'});
+    });
 });
 
 router.get('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const postRouter = require('./posts/postRouter.js');
+const userRouter = require('./users/userRouter.js');
+
 
 const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+
+// endpoints
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
